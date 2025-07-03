93394b1d2c230f258e93db53f145cab637ac7318
Abdullah khalid <91344658+abduIIahKhaIid@users.noreply.github.com>
2025-07-03 15:22:52 +0500

    Create simple-interest.sh

---
+++ github/simple-interest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+echo "Enter Principal amount:"
+read p
+echo "Enter Rate of interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+echo "Simple Interest is: $si"
